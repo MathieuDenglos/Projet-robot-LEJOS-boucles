9e408d5f12ca349f206de2c2c541a977f6e3cfef
Mathieu Denglos <matdenglos@gmail.com>
2021-04-01 21:14:08 +0200

    remove features past Java 6

--- src/Labyrinthe.java
+++ src/Labyrinthe.java
@@ -2,18 +2,17 @@ import lejos.nxt.ColorSensor.Color;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Arrays;
+import lejos.robotics.pathfinding.AstarSearchAlgorithm;
 
 public class Labyrinthe {
-    private Noeud entree;
+    private Noeud entree, tresor; // Contient donc tout le labyrinthe
     private Map<Noeud, Orientation> noeuds_communs = new HashMap<>();
-    // Potentiellement une série de Node de Lejos pour le Astar
 
-    public Orientation dans_la_map(Noeud comparaison) {
+    public Orientation chercher_noeud_commun(Noeud comparaison) {
         return noeuds_communs.get(comparaison);
     }
 
-    public void ajout_dans_map(Noeud noeud, Orientation orientation) {
+    public void ajout_noeud_commun(Noeud noeud, Orientation orientation) {
         noeuds_communs.put(noeud, orientation);
     }
 
@@ -26,16 +25,21 @@ public class Labyrinthe {
         robot.calibration();
 
         // création du premier lieu avec un couloir dans le sens du robot
-        entree = new Noeud(new Color(0, 0, 0, 0, 0),
-                new ArrayList<>(Arrays.asList(new Couloir[] { new Couloir(robot.get_orientation()) })));
+        ArrayList<Couloir> temp = new ArrayList<>();
+        temp.add(new Couloir(robot.get_orientation()));
+        entree = new Noeud(new Color(0, 0, 0, 0, 0), temp, (float) robot.get_x(), (float) robot.get_y());
 
         // appel de la fonction récursif qui va créer les noeuds
         entree.get_couloirs().get(0).visite_couloir(robot, this);
 
         // Résolution du chemin si le trésor a été trouvé
         // sinon le robot se situe déjà à l'entrée du labyrinthe
-        if (robot.get_tresor_trouve())
-            ; // résolution
+        if (tresor != null) {
 
+        }
+    }
+
+    public void set_noeud_tresor(Noeud noeud) {
+        this.tresor = noeud;
     }
 }
